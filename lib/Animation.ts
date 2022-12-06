05090b1b7b650fc4b3746433c92ced6cd4765af9
He Linming <hlm52pk@163.com>
2022-12-06 17:39:42 +0800

    feat: store velocity state

--- lib/Animation.ts
+++ lib/Animation.ts
@@ -1,24 +1,34 @@
-import { animate as popAnimate, AnimationOptions } from "popmotion";
+import {
+  animate as popAnimate,
+  AnimationOptions,
+  SpringOptions,
+} from "popmotion";
 
 const noop = () => {};
 
 export class Animation {
-  private state?: number;
+  private state: number;
+  private velocity: number;
   private stop = noop;
-  constructor(state?: number) {
-    this.state = state;
+  constructor(state?: number, velocity?: number) {
+    this.state = state ?? 0;
+    this.velocity = velocity ?? 0;
   }
 
   animate = (options: AnimationOptions<number>) => {
     this.stop = popAnimate({
       ...options,
       from: this.state,
+      velocity: Object.hasOwn(options, "velocity")
+        ? (options as SpringOptions).velocity
+        : this.velocity,
       onPlay: () => {
         // Stop last animation
         this.stop();
         options.onPlay?.();
       },
       onUpdate: (latest: number) => {
+        this.velocity = latest - this.state;
         this.state = latest;
         options.onUpdate?.(latest);
       },
